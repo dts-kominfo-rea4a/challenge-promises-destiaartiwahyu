cca946af1476938d3dc0a46fffb303b4ab220471
destiaartiwahyu <destiaartiwahyu@gmail.com>
2022-09-20 20:48:02 +0700

    modify promise js

--- promise.js
+++ promise.js
@@ -1,7 +1,32 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const dataTheaterIXX = await promiseTheaterIXX();
+  const dataTheaterVGC = await promiseTheaterVGC();
+
+  const dataEmosiTheaterIXX = dataTheaterIXX.filter(
+    (data) => data.hasil === emosi
+  );
+  const dataEmosiTheaterVGC = dataTheaterVGC.filter(
+    (data) => data.hasil === emosi
+  );
+
+  return dataEmosiTheaterIXX.length + dataEmosiTheaterVGC.length;
+
+  console.log(
+    "data Theater IXX setelah difilter",
+    dataEmosiTheaterIXX,
+    "data Theater VGC setelah difilter",
+    dataEmosiTheaterVGC,
+    "hitung teater IXX",
+    dataEmosiTheaterIXX.length,
+    "hitung teater VGC",
+    dataEmosiTheaterVGC.length,
+    "totalnya",
+    dataEmosiTheaterIXX.length + dataEmosiTheaterVGC.length
+  );
+};
 
 module.exports = {
   promiseOutput,
